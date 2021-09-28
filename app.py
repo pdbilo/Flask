eb90ef548f499777ae05ef3f25b56aac75b2cad6
pdbilo <bilocopetiuc21@gmail.com>
2021-09-28 14:33:42 -0300

    V0.03

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request, url_for, redirect
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+
 
 app=Flask(__name__)
 
+@app.before_request
+def before_request():
+    print("antes de la peticion")
+
+@app.after_request
+def after_request(response):
+    print("despues la peticion")
+    return response 
+    
 @app.route('/')
 def index():
     cursos=["php","python","Java","Kotlin","Dart","Javascript"]
@@ -13,8 +25,29 @@ def index():
     }
     return render_template('index.html',data=data)
 
+@app.route('/contacto/<nombre>/<int:edad>')
+def contacto(nombre,edad):
+    data={
+        'titulo':'contacto',
+        'nombre': nombre,
+        'edad': edad
+    }
+    return render_template("contacto.html",data=data)
+
+def query_string():
+    print(request)
+    print(request.args)
+    print(request.args.get('param1'))
+    print(request.args.get('param2'))
+    return "ok"
+
+def pagina_no_encontrada(error):
+#    return render_template('404.html'),404 
+    return redirect(url_for('index'))
 
 if __name__=='__main__':
+    app.add_url_rule('/query_string',view_func=query_string)
+    app.register_error_handler(404,pagina_no_encontrada)
     app.run(debug=True,port=5000)
 
 
