7bb1f2ff07a7bcceb36babc94320d3edeca215bd
pdbilo <bilocopetiuc21@gmail.com>
2021-09-28 13:47:49 -0300

    V0.02

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    cursos=["php","python","Java","Kotlin","Dart","Javascript"]
+    data={
+        "titulo":"index123",
+        "bienvenida":"saludos!",
+        "cursos":cursos,
+        "numero_cursos":len(cursos)
+    }
+    return render_template('index.html',data=data)
+
+
+if __name__=='__main__':
+    app.run(debug=True,port=5000)
+
+
